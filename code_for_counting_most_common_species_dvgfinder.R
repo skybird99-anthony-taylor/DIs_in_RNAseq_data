268f74093f4e1eb37d798d17cd3d40d1506d2c3e
Anthony Taylor <102563589+skybird99-anthony-taylor@users.noreply.github.com>
2024-12-16 16:00:07 -0500

    Update and rename code_for_counting_most_common_species_dvgfinder_excel.R to code_for_counting_most_common_species_dvgfinder.R

---
+++ code_for_counting_most_common_species_dvgfinder.R
@@ -0,0 +1,45 @@
+# Load necessary libraries
+library(readr)
+library(dplyr)
+library(writexl)
+library(tidyr)
+library(tidyverse)
+
+# Import combined file
+
+# Create new df using the two relevant columns from the combined file
+junctions <- combined_deletions %>% select(BP,RI)
+junctions$ID <- paste(junctions$BP, junctions$RI, sep = "_")
+
+# Count occurrences of each Coordinate ID and append as a new column, arrange in descending order
+junctions <- junctions %>%
+  group_by(ID) %>%
+  mutate(Count = n()) %>%
+  ungroup() %>%
+  arrange(desc(Count))
+
+# Keep only one instance of each coordinate ID
+junctions_filtered <- junctions %>%
+  distinct(ID, .keep_all = TRUE)  # Keep one instance of each Coordinate_ID
+
+# Click on df and remove all counts smaller than the largest value
+junctions_filtered <- junctions_filtered %>%
+  filter(Count >= 2) %>%  # Click on the df to see what the max number is, and set that
+  distinct(ID, .keep_all = TRUE) 
+
+# Histogram + dplyr filters for help manually binning. The goal is to whittle down to the biggest bin.
+# If all the BP is the vame value but the RI is different, this is useful. Same if it's vice-versa.
+# Otherwise you have to fiddle with it.
+ggplot(junctions_filtered, aes(x = RI)) +
+  geom_histogram(binwidth = 10, fill = "skyblue", color = "black") +
+  stat_bin(
+    binwidth = 10, 
+    aes(label = after_stat(count)), 
+    geom = "text", 
+    vjust = -0.5, 
+    color = "red"
+  ) +
+  labs(title = "Histogram with Bin Labels", x = "RI", y = "Frequency")
+
+junctions_filtered <- junctions_filtered %>%
+  filter(RI < 3600) # Example RI here is 3,600
